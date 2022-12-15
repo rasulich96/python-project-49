9d10f4f341540c30987ba146a0e3514337a013b4
Magomedrasul Kurbanov <rasulich96@yandex.ru>
2022-12-15 19:02:37 +0000

    fixed prime.py game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,24 +5,25 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    result = 'yes'
     # Т.к. делитель не может быть больше половины самого числа,
     # зараннее разделим его на 2, так сократим количество итераций
     div = number // 2
     while div > 1:
         if number == 1:
-            result = 'no'
-            return result
+            return False
         if number % div == 0:
-            result = 'no'
-            return result
+            return False
         else:
             div -= 1
-    return result
+    return True
 
 
 def build_game():
     random_number = random.randint(1, 99)
     question = f'Question: {random_number}'
     result = is_prime(random_number)
+    if is_prime(random_number) is True:
+        result = 'yes'
+    else:
+        result = 'no'
     return result, question
